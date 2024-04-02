3b67427c16225a1c7f29171031b99fefaf50bec0
Daniel Exposito <alu0101481876@ull.edu.es>
2024-04-02 21:58:45 +0000

    Añadido tests

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -12,11 +12,32 @@ describe('MessageEventEmitterClient', () => {
     client.on('message', (message) => {
       expect(message).to.be.eql({ 'type': 'command', 'content': 'ls' });
       expect(message).to.be.eql({ 'type': message.type, 'content': message.content });
+      expect(message.type).to.be.equal('command');
+      expect(message.content).to.be.equal('ls');
       done();
     });
 
     socket.emit('data', '{"type": "command" ');
     socket.emit('data', ', "content": "ls"}');
+    socket.emit('data', '\n');
+  });
+});
+
+describe('MessageEventEmitterClient', () => {
+  it('Deberia el cliente devolver el json', (done) => {
+    const socket = new EventEmitter();
+    const client = new MessageEventEmitterClient(socket);
+
+    client.on('message', (message) => {
+      expect(message).to.be.eql({ 'type': 'respuesta', 'content': 'ls -la' }); 
+      expect(message).to.be.eql({ 'type': message.type, 'content': message.content });
+      expect(message.type).to.be.equal('respuesta');
+      expect(message.content).to.be.equal('ls -la');
+      done();
+    });
+
+    socket.emit('data', '{"type": "respuesta" ');
+    socket.emit('data', ', "content": "ls -la"}');
     socket.emit('data', '\n');
   });
 });
